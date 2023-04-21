4809ad367bbe6caac370aa8a3b061673f555ccd8
Mister Diallo <77695206+misterdiallo@users.noreply.github.com>
2023-04-21 12:09:01 +0800

    Adding Kafka Config and the CustomerVisitEventModel

---
+++ src/main/java/com/misterdiallo/backend/springkafka/domain/CustomerVisitEvent.java
@@ -0,0 +1,17 @@
+package com.misterdiallo.backend.springkafka.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CustomerVisitEvent {
+    private String customerId;
+    private LocalDateTime dateTime;
+}
